212c6644e64555048d23d9fa68d7c808a07cf54c
Thomas Burton <tburton5572@gmail.com>
2022-04-01 13:47:49 +0100

    simplified package structure

--- src/FieldsOptimiser.jl
+++ src/FieldsOptimiser.jl
@@ -1,5 +1,45 @@
 module FieldsOptimiser
 
+using NSOperators
+using Projector
 
+# TODO: use "optimtest" to make sure gradient is compatible
+
+export optimize
+
+# TODO: add projection step to this function
+function myfinalize!(x, f, g, numiter)
+    # perform projection
+    
+
+    return x, f, g
+end
+
+function init_fg(cache::Cache)
+    function fg(U)
+        update_v!(U, cache)
+        update_p!(cache)
+        localresidual!(U, cache)
+        update_r!(cache)
+        return ℜ(cache), dℜ!(cache) 
+    end
+end
+
+# TODO: how is optimize called properly?
+function optimize(U₀::V, mean::NTuple{4, Vector{T}}; algorithm=ConjugateGradient()) where {T, S<:AbstractArray{Complex{T}, 3}, V<:AbstractVector{S}}
+    # initialise cache
+    # TODO: initialise cache properly
+    _cache = 1.0
+
+    # define optimiser function
+    function ℜdℜ(U; cache=_cache)
+        r = localresidual!(U, cache)
+        return ℜ(r), dℜ!(r, cache)
+    end
+
+    # run optimisation
+    # FIXME: method call error?
+    optimize(ℜdℜ, U₀, algorithm; finalize! = myfinalize!)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,22 @@
 using Test
 
+# TODO: test for this function
+@testset "ℜdℜ function      " begin
+    # construct velocity field (incompressible and no-slip)
 
+    # initialise fg()
+
+    # compute residual and gradient using fg()
+
+    # compute residual and gradient using functions
+
+    # compare
+end
+
+@testset "Optimize decrease " begin
+    # construct velocity field (incompressible and no-slip)
+
+    # perform single iteration of optimisation
+
+    # check function value has decreased
+end

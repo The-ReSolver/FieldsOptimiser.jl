cde6327887294c1b7a65f7bbe943d882dcfbc568
Thomas Burton <tburton5572@gmail.com>
2022-04-21 14:53:30 +0100

    tested finaliser function

--- src/FieldsOptimiser.jl
+++ src/FieldsOptimiser.jl
@@ -1,21 +1,45 @@
 module FieldsOptimiser
 
+using OptimKit
+using Printf
+using LinearAlgebra
+
 using NSOperators
 using Projector
 
 # TODO: use "optimtest" to make sure gradient is compatible
+# TODO: still need to figure out way to structure this to avoid using my packages
+# TODO: access fields stored in cache using proper interface (methods???)
+# TODO: can construct fields using type parameter (need to implement appropriate constructor tho)
 
 export optimize
 
-# TODO: add projection step to this function
-function myfinalize!(x, f, g, numiter)
-    # perform projection
-    
+function init_myfinalize!(leray!, slipcorrector!, cache, io::IO=stdout)
+    # initialise temporary field
+    div_U = similar(cache.spec_cache[1])
 
-    return x, f, g
+    function myfinalize!(U, ℜ, dℜ, numiter)
+        # calculate divergence
+        div_U = cache.spec_cache[11] .+ cache.spec_cache[6]
+        @views begin
+            slip_norm = sqrt(norm(U[1, :, :])^2 + norm(U[end, :, :])^2)
+            res_norm = sqrt(norm(cache.spec_cache[36][1, :, :])^2 + norm(cache.spec_cache[36][end, :, :])^2 +
+                            norm(cache.spec_cache[37][1, :, :])^2 + norm(cache.spec_cache[37][end, :, :])^2 +
+                            norm(cache.spec_cache[38][1, :, :])^2 + norm(cache.spec_cache[38][end, :, :])^2)
+        end
+
+        # print relevant statistics
+        @printf io "||∇U|| = %0.6f     slip_norm = %0.6f     res_norm = %0.6f\n" norm(div_U) slip_norm res_norm
+
+        # perform projections
+        leray!(U)
+        slipcorrector!(U)
+
+        return U, ℜ, dℜ
+    end
 end
 
-function init_ℜdℜ(cache::Cache)
+function init_ℜdℜ(cache::C) where {C}
     function ℜdℜ(U)
         update_v!(U, cache)
         update_p!(cache)
@@ -25,16 +49,20 @@ function init_ℜdℜ(cache::Cache)
     end
 end
 
-# TODO: how is optimize called properly?
-function optimize(U₀::V, mean::NTuple{4, Vector{T}}; algorithm=ConjugateGradient()) where {T, S<:AbstractArray{Complex{T}, 3}, V<:AbstractVector{S}}
+# TODO: can the type parameter stuff be done with a different pattern?
+function optimize(U₀::V, mean::NTuple{3, Vector{T}}, Re::T, Ro::T; algorithm=ConjugateGradient()) where {T, S<:AbstractArray{Complex{T}, 3}, V<:AbstractVector{S}}
     # initialise cache
-    # TODO: initialise cache properly
-    _cache = 1.0
+    _cache = Cache(U₀[1].grid, mean..., Re, Ro)
+
+    # initialise projections
+    leray! = Leray!(U₀[1].grid)
+    slipcorrector! = SlipCorrector!(U₀[1].grid)
+
+    # initialise finalisation function
 
     # initialise objective function
 
     # run optimisation
-    # FIXME: method call error?
     optimize(ℜdℜ, U₀, algorithm; finalize! = myfinalize!)
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,14 @@
 using FieldsOptimiser
+
 using Test
+using Random
 
 using ChebUtils
 using Fields
 using NSOperators
+using Projector
 
-@testset "ℜdℜ function      " begin
+@testset "ℜdℜ function              " begin
     # construct velocity field (incompressible and no-slip)
     Ny = 64; Nz = 64; Nt = 64
     y = chebpts(Ny)
@@ -55,8 +58,93 @@ using NSOperators
     @test dℜ_opt == dℜ_man
 end
 
-@testset "Optimize decrease " begin
+@testset "Projection finalisation   " begin
+    # initialise velocity field (compressible and slipping)
+    Ny = 64; Nz = 64; Nt = 64
+    y = chebpts(Ny)
+    Dy = chebdiff(Ny)
+    Dy2 = chebddiff(Ny)
+    ws = chebws(Dy)
+    ω = 1.0
+    β = 1.0
+    u_fun(y, z, t) = (y^2)*exp(cos(z))*atan(sin(t))
+    v_fun(y, z, t) = sin(π*y)*exp(sin(z))*atan(cos(t))
+    w_fun(y, z, t) = cos(π*y)*cos(z)*exp(cos(t))
+    grid = Grid(y, Nz, Nt, Dy, Dy2, ws, ω, β)
+    u = VectorField(PhysicalField(grid, u_fun),
+                    PhysicalField(grid, v_fun),
+                    PhysicalField(grid, w_fun))
+    U = VectorField(grid)
+    FFT! = FFTPlan!(grid; flags=ESTIMATE)
+    FFT!(U, u)
+
+    # calculate divergence of velocity field
+    dVdy = SpectralField(grid)
+    dWdz = SpectralField(grid)
+    ddy!(U[2], dVdy)
+    ddz!(U[3], dWdz)
+    divU = dVdy + dWdz
+    
+    # initialise projections and cache
+    leray! = Leray!(grid)
+    slipcorrector! = SlipCorrector!(grid)
+    cache = Cache(grid, rand(Ny), rand(Ny), rand(Ny), 1.0, 1.0)
+
+    # initialise finaliser function and objective function
+    tmp_io = IOBuffer()
+    myfinalize! = FieldsOptimiser.init_myfinalize!(leray!, slipcorrector!, cache, tmp_io)
+    ℜdℜ = FieldsOptimiser.init_ℜdℜ(cache)
+
+    # calculate residual and gradient
+    ℜ, dℜ = ℜdℜ(U)
+
+    # run finalizer
+    U, ℜ, dℜ = myfinalize!(U, ℜ, dℜ, 1)
+
+    # calculate divergence of new velocity field
+    ddy!(U[2], dVdy)
+    ddz!(U[3], dWdz)
+    divU = dVdy + dWdz
+    @test norm(divU) < 1e-9
+
+    # test slip
+    @test U[1][1, :, :] ≈ zeros((Nz >> 1) + 1, Nt) atol=1e-12
+    @test U[1][end, :, :] ≈ zeros((Nz >> 1) + 1, Nt) atol=1e-12
+    @test U[2][1, :, :] ≈ zeros((Nz >> 1) + 1, Nt) atol=1e-12
+    @test U[2][end, :, :] ≈ zeros((Nz >> 1) + 1, Nt) atol=1e-12
+    @test U[3][1, :, :] ≈ zeros((Nz >> 1) + 1, Nt) atol=1e-7
+    @test U[3][end, :, :] ≈ zeros((Nz >> 1) + 1, Nt) atol=1e-7
+end
+
+@testset "Optimize decrease         " begin
     # construct velocity field (incompressible and no-slip)
+    Ny = 64; Nz = 64; Nt = 64
+    y = chebpts(Ny)
+    Dy = chebdiff(Ny)
+    Dy2 = chebddiff(Ny)
+    ws = chebws(Dy)
+    ω = 1.0
+    β = 1.0
+    grid = Grid(y, Nz, Nt, Dy, Dy2, ws, ω, β)
+    u_fun(y, z, t) = sin(π*y)*exp(cos(z))*sin(t)
+    v_fun(y, z, t) = (cos(π*y) + 1)*cos(z)*sin(t)
+    w_fun(y, z, t) = π*sin(π*y)*sin(z)*sin(t)
+    u = VectorField(PhysicalField(grid, u_fun),
+                    PhysicalField(grid, v_fun),
+                    PhysicalField(grid, w_fun))
+    U = VectorField(grid)
+    FFT! = FFTPlan!(grid; flags=UInt32(64)) # FFTW.ESTIMATE = UInt32(64)
+    FFT!(U, u)
+    Re = abs(rand())
+    Ro = abs(rand())
+
+    # construct laminar mean field
+    ū_fun(y) = y
+    dūdy_fun(y) = 1.0
+    d2ūdy2_fun(y) = 0.0
+    ū = [ū_fun(y[i]) for i in 1:Ny]
+    dūdy = [dūdy_fun(y[i]) for i in 1:Ny]
+    d2ūdy2 = [d2ūdy2_fun(y[i]) for i in 1:Ny]
 
     # perform single iteration of optimisation
 
